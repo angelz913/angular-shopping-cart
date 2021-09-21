e07980afde756defe0727f075b894864bf13121c
Angel Zhang <anzhouzhang913@gmail.com>
2021-09-20 22:45:39 -0400

    Add comments

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -4,14 +4,15 @@ import { Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
-  selector: 'app-product-alerts', // the selector identifies the component
+  selector: 'app-product-alerts', // the selector identifies the component, "app-" + name
   templateUrl: './product-alerts.component.html', // the component's HTML
   styleUrls: ['./product-alerts.component.css'], // the component's CSS
 })
 export class ProductAlertsComponent implements OnInit {
+  // the property value passes in from the component's parent, ProductListComponent
   @Input() product!: Product;
+  // emit an event when the value of the notify property changes
   @Output() notify = new EventEmitter();
   constructor() {}
-
   ngOnInit() {}
 }
